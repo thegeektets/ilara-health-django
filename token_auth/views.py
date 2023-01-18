53f6f3c454ecf7bf8be60ea69833d5884901cd3d
Griffin Muteti <griffinmuteti31@gmail.com>
2023-01-18 23:36:39 +0300

    authentication
    - respond with token on login

--- token_auth/views.py
+++ token_auth/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Token
+from django.utils.crypto import get_random_string
+from django.contrib.auth import get_user_model
 
 
 @api_view(['POST'])
@@ -12,20 +14,24 @@ def login(request):
 
     user = authenticate(request, email=email, password=password)
 
-    print(user)
+    print(user.id)
 
     if user is not None:
-        # login(request._request, user)
         # generate and return the token
-        token, created = Token.objects.get_or_create(user=user)
+        unique_id = get_random_string(length=32)
+     
+        print(user)
 
-        if not created:
-                # update the created time of the token to keep it valid
-                token.created = datetime.datetime.now(datetime.timezone.utc)
-                token.save()
-        
-        print(token)
-        
+        token = Token.objects.get(user_id=user.id)
+
+        if not token:
+            # update the created time of the token to keep it valid
+            created = datetime.datetime.now(datetime.timezone.utc)
+
+            token = Token.objects.create(
+            user=user, key=unique_id, created=created)
+
+
         return Response({'token': token.key})
     else:
         return Response({'error': 'Invalid credentials'}, status=400)
