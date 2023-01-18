3877d99f9fefe0b9842cc7a0efc79f480750a2e6
Griffin Muteti <griffinmuteti31@gmail.com>
2023-01-18 12:57:29 +0300

    urls

--- customer/urls.py
+++ customer/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
-from rest_framework import routers
-from .views import CustomerViewSet, AddressViewSet
+from .views import CustomerViewSet
 
-router = routers.DefaultRouter()
-router.register(r'customers', CustomerViewSet)
+customer_list = CustomerViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+customer_detail = CustomerViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'delete': 'destroy'
+})
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('customers/', customer_list, name='customer-list'),
+    path('customers/<int:pk>/', customer_detail, name='customer-detail'),
 ]
