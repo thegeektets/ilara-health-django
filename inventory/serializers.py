3631f340ec4ade5ed040fc57e92fa44ff69562db
Griffin Muteti <griffinmuteti31@gmail.com>
2023-01-19 12:11:51 +0300

    add products
    - update serializers to handle create product

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,13 +1,34 @@
 from rest_framework import serializers
 from .models import Product, Category
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('id', 'name', 'price', 'description')
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ('id', 'name')
 
+    def create(self, validated_data):
+        category = Category.objects.create(**validated_data)
+        return category
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'price', 'quantity', 'description', 'category')
+
+    def create(self, validated_data):
+        category_data = validated_data.pop('category')
+        category_name = category_data.get('name', None) 
+        
+        if category_name is None:
+            raise serializers.ValidationError(
+                {'category': 'This field is required.'})
+        try:
+            category = Category.objects.get(name=category_name)
+        except Category.DoesNotExist:
+            raise serializers.ValidationError({'category': 'Invalid category id'})
+        product = Product.objects.create(category=category, **validated_data)
+        return product
