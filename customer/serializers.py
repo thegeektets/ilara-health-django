c52535ef5faf5c4d80e34e51283ef6683a5a8230
Griffin Muteti <griffinmuteti31@gmail.com>
2023-01-19 13:25:32 +0300

    Customers
    - creat new user on customers if customer is not defined

--- customer/serializers.py
+++ customer/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Customer
 
 class CustomerSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(required=False, queryset=User.objects.all())
+
     class Meta:
         model = Customer
         fields = '__all__'
+
+    def create(self, validated_data):
+        if 'user' not in validated_data:
+            user = User.objects.create(username=validated_data['email'])
+            validated_data['user'] = user
+        return super().create(validated_data)
--- inventory/serializers.py
+++ inventory/serializers.py
@@ -8,6 +8,15 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
 
     def create(self, validated_data):
+        category_name = validated_data.get('name', None) 
+        
+        if category_name is None:
+            raise serializers.ValidationError(
+                {'category': 'This field is required.'})
+        try:
+            category = Category.objects.get(name=category_name)
+        except Category.DoesNotExist:
+            raise serializers.ValidationError({'category': 'Invalid category id'})
         category = Category.objects.create(**validated_data)
         return category
 
@@ -32,3 +41,20 @@ class ProductSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({'category': 'Invalid category id'})
         product = Product.objects.create(category=category, **validated_data)
         return product
+    
+    def update(self, instance, validated_data):
+        category_data = validated_data.pop('category')
+        category_name = category_data.get('name',None)
+        if category_name is None:
+            raise serializers.ValidationError({'category': 'This field is required.'})
+        try:
+            category = Category.objects.get(name=category_name)
+        except Category.DoesNotExist:
+            raise serializers.ValidationError({'category': 'Invalid category id'})
+        instance.category = category
+        instance.name = validated_data.get('name', instance.name)
+        instance.price = validated_data.get('price', instance.price)
+        instance.quantity = validated_data.get('quantity', instance.quantity)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        return instance
